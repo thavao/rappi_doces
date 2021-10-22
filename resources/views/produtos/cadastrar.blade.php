541b293dae16cbb62ede780be4cd715fa1569a4f
matheus04cell <matheusvcnt04@gmail.com>
2021-10-22 02:01:16 -0300

    Alteração visual das paginas de cadastro, edição e show dos produtos.

--- resources/views/produtos/cadastrar.blade.php
+++ resources/views/produtos/cadastrar.blade.php
@@ -10,39 +10,41 @@
 
 @if (Auth::user()->nivel < 10)
 <div>
-  <h1>Cadastre um produto</h1>
+  <div class="flex justify-center items-center w-full bg-white ">
+    <div class="w-1/2 bg-green-600 rounded .shadow ... p-8 m-4">
+        <h1 class="block w-full text-center text-white text-2xl font-bold mb-6">Cadastro de Produtos</h1>
   <form action="/cadastrar" method="POST" enctype="multipart/form-data">
     @csrf
-    <div class="form-group">
-      <label for="title">Nome do Produto: </label>
-      <input type="text"  id="NomeProduto" name="NomeProduto" placeholder="Nome do produto">
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="NomeProduto">Nome do Produto</label>
+      <input class="border py-1 px-2 text-grey-800" type="text"  id="NomeProduto" name="NomeProduto" placeholder="Nome do produto">
     </div>
-    <div class="form-group">
-      <label for="title">Preço: </label>
-      <input type="float"  id="preco" name="preco" placeholder="R$ 00.0">
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="title">Valor do Produto</label>
+      <input class="border py-2 px-3 text-grey-800" type="float"  id="preco" name="preco" placeholder="R$ 00.00">
     </div>
 
-    <div class="form-group">
-      <label for="title">Descrição: </label>
-      <textarea name="descricao" id="descricao"  placeholder="Descrição"></textarea>
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="descricao">Descrição</label>
+      <textarea class="border py-2 px-3 text-grey-800" name="descricao" id="descricao"  placeholder="Descrição"></textarea>
     </div>
-    <div class="form-group">
-        <label for="title">Quantidade de estoque: </label>
-        <input type="int"  id="qtdestoque" name="qtdestoque" placeholder="00000">
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="title">Quantidade de Estoque</label>
+        <input class="border py-2 px-3 text-grey-800" type="int"  id="qtdestoque" name="qtdestoque" placeholder="00000">
       </div>
-      <div class="form-group">
-        <label for="title">Estoque Mínimo: </label>
-        <input type="int"  id="Esquetomin" name="Estoquemin" placeholder="00000">
+      <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="title">Estoque Mínimo</label>
+        <input class="border py-2 px-3 text-grey-800" type="int"  id="Esquetomin" name="Estoquemin" placeholder="00000">
       </div>
-      <div class="form-group">
-        <label for="title">ID do Fornecedor: </label>
-        <input type="int"  id="Fornecedor_id" name="Fornecedor_id" placeholder="00000">
+      <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="title">ID do Fornecedor</label>
+        <input class="border py-2 px-3 text-grey-800" type="int"  id="Fornecedor_id" name="Fornecedor_id" placeholder="00000">
       </div>
-      <div class="form-group">
-        <label for="image">Imagem do produto: </label>
+      <div class="flex flex-col mb-4">
+                <label class="mb-2 font-bold text-lg text-white" for="image">Imagem do Produto</label>
         <input type="file" id="imagem" name="imagem">
       </div>
-    <input type="submit"  value="Finalizar Cadastro">
+    <input class="block bg-green-700 hover:bg-green-800 text-white uppercase text-lg mx-auto p-4 rounded" type="submit"  value="Finalizar Cadastro">
   </form>
 </div>
 <script>
--- resources/views/produtos/editar.blade.php
+++ resources/views/produtos/editar.blade.php
@@ -10,42 +10,44 @@
 
 @if (Auth::user()->nivel < 10)
 <div>
-  <h1>Editando: {{$produto->NomeProduto}}</h1>
+  <div class="flex justify-center items-center w-full bg-white ">
+    <div class=  "w-1/2 bg-green-600 rounded .shadow ... p-8 m-4">
+        <h1 class="md:container md:mxauto px-4 bg-green-700  block w-full text-center text-white text-2xl font-bold mb-6">Editando: {{$produto->NomeProduto}}</h1>
   <form action="/produtos/update/{{$produto->id}}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('PUT')
-    <div class="form-group">
-      <label for="title">Nome do Produto: </label>
-      <input type="text"  id="NomeProduto" name="NomeProduto" placeholder="Nome do produto" value="{{$produto->NomeProduto}}">
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="NomeProduto">Nome do Produto:</label>
+      <input class="border py-1 px-2 text-grey-800" type="text"  id="NomeProduto" name="NomeProduto" placeholder="Nome do produto" value="{{$produto->NomeProduto}}">
     </div>
-    <div class="form-group">
-      <label for="title">Preço:</label>
-      <input type="float"  id="preco" name="preco" placeholder="R$ 00.0" value="{{$produto->preco}}" >
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="preco">Valor do Produto:</label>
+      <input class="border py-1 px-2 text-grey-800" type="float"  id="preco" name="preco" placeholder="R$ 00.0" value="{{$produto->preco}}" >
     </div>
 
-    <div class="form-group">
-      <label for="title">Descrição:</label>
-      <textarea name="descricao" id="descricao"  placeholder="Descrição" value="{{$produto->descricao}}"></textarea>
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="descricao">Descrição:</label>
+      <input autocapitalize="descricao" class="border py-1 px-2 text-grey-800" name="descricao" id="descricao"  placeholder="Descrição" value="{{$produto->descricao}}"></input>
     </div>
-    <div class="form-group">
-        <label for="title">Quantidade de estoque:</label>
-        <input type="int"  id="qtdestoque" name="qtdestoque" placeholder="00000" value="{{$produto->qtdestoque}}">
+    <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="qtdestoque">Quantidade de Estoque:</label>
+        <input class="border py-1 px-2 text-grey-800" type="int"  id="qtdestoque" name="qtdestoque" placeholder="00000" value="{{$produto->qtdestoque}}">
       </div>
-      <div class="form-group">
-        <label for="title">Estoque Mínimo:</label>
-        <input type="int"  id="Esquetomin" name="Estoquemin" placeholder="00000"value="{{$produto->Estoquemin}}">
+      <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="">Estoque Mínimo:</label>
+        <input class="border py-1 px-2 text-grey-800" type="int"  id="Estoquemin" name="Estoquemin" placeholder="00000"value="{{$produto->Estoquemin}}">
       </div>
-      <div class="form-group">
-        <label for="title">ID do Fornecedor:</label>
-        <input type="int"  id="Fornecedor_id" name="Fornecedor_id" placeholder="00000" value="{{$produto->Fornecedor_id}}">
+      <div class="flex flex-col mb-4">
+        <label class="mb-2 font-bold text-lg text-white" for="Fornecedor_id">ID do Fornecedor:</label>
+        <input class="border py-1 px-2 text-grey-800" type="int"  id="Fornecedor_id" name="Fornecedor_id" placeholder="00000" value="{{$produto->Fornecedor_id}}">
       </div>
 
-      <div class="form-group">
-        <img src="/produtos/{{$produto->imagem}}" alt="imagem de {{$produto->NomeProduto}}">
-        <label for="image">Imagem do produto:</label>
-        <input type="file" id="imagem" name="imagem" >
+      <div class="flex flex-col mb-4">
+        <img class="object-scale-down h-48 w-full" src=" /produtos/{{$produto->imagem}}" alt="imagem de {{$produto->NomeProduto}}">
+        <label class="mb-2 font-bold text-lg text-white" for="image">Imagem do Produto</label>
+        <input class="border py-1 px-2 text-grey-800" type="file" id="imagem" name="imagem" >
       </div>
-    <input type="submit"  value="Finalizar Edição">
+    <input class="block bg-green-700 hover:bg-green-800 text-white uppercase text-lg mx-auto p-4 rounded" type="submit" value="Finalizar Edição">
   </form>
 </div>
 <script>
